6e57200a14cdf2e7284759ecced00b2af10fb278
Damith Welagedara <dnsuriya@gmail.com>
2022-11-17 22:58:47 +1300

    d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "53fb59e54fb87bb44dba7c2200e1dead",
-"index.html": "a934ca83bf5fa7f26689221864e22fec",
-"/": "a934ca83bf5fa7f26689221864e22fec",
-"main.dart.js": "ec0d19cb6fc325f15b1fe91f28ae37ff",
+"index.html": "e25679d52c400d80da44236ea6ded867",
+"/": "e25679d52c400d80da44236ea6ded867",
+"main.dart.js": "cd9ce8b1f0cd855fcd7d755c0a82a5b0",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
